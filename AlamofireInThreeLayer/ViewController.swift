11951f262234a2a35a28423db64779df78c9e8b4
mahdifarshid <farshidapple4@gmail.com>
2019-03-27 12:54:11 +0430

    Initial Commit

---
+++ AlamofireInThreeLayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlamofireInThreeLayer
+//
+//  Created by farshid on 27.03.19.
+//  Copyright © 2019 com.mahdifarshid.Alamofire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
